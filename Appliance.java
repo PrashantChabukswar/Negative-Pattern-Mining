aaa532c7b1a451ba9667c41a701f083e0e5a627a
PrashantChabukswar <80749778+PrashantChabukswar@users.noreply.github.com>
2021-04-11 22:42:41 +0530

    Delete Appliance.java

--- Appliance.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Appliance
-{
-	private int applianceId;
-	private string applinaceName;
-	private String applinaceCategory;
-	private double applianceAmount;
-	private booolean insurance;
-	
